fcf87474115dbdaf9c93c39016ad0d217aa86ef7
morganamilo <morganamilo@gmail.com>
2018-06-01 17:09:09 +0100

    UAWSOD: fix night vesion not working
    PlayerStandard._get_input apears to alter the input state. When we call
    _get_input it causes the next _get_input call to behave
    differently than expected. Notablly breaking night vision, but possibly
    other things.
    
    Instead, after we do our initial call to _get_input, cache the answer and
    wrap _get_input to just return our cached result. Restore it to the
    original function after.

--- use-ability-while-shooting-or-driving/playerstandard.lua
+++ use-ability-while-shooting-or-driving/playerstandard.lua
@@ -8,12 +8,18 @@ function PlayerStandard:_update_check_actions(t, dt, paused)
     
 	if projectile_tweak.ability then
 		local _check_action_use_ability = PlayerStandard._check_action_use_ability
+		local getInput = PlayerStandard._get_input
 		local input = self:_get_input(t, dt, paused)
+
+		PlayerStandard._get_input = function(t, dt, paused)
+			return input
+		end
 		
 		self:_check_action_use_ability(t, input)
 		PlayerStandard._check_action_use_ability = nop
 		_update_check_actions(self, t, dt, paused)
 		PlayerStandard._check_action_use_ability = _check_action_use_ability
+		PlayerStandard._get_input = getInput
 	else
 		_update_check_actions(self, t, dt, paused)
 	end
